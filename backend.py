6314f78a4ab1d780c91420aa7da93327687db97c
johanvenbakken <johanhven@gmail.com>
2025-06-11 22:36:31 +0200

    a

--- backend.py
+++ backend.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 from cryptography.fernet import Fernet
 from flask_socketio import SocketIO, emit
 from flask_mail import Mail, Message
-from itsdangerous import URLSafeTimedSerializer
+
 
 load_dotenv()
 
